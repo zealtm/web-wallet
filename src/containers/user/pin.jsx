848980decb35ea0e7e5f82a300ffc03d7449c264
Hellen <hellen.ferreira@lunes.io>
2018-07-12 15:33:47 -0300

    pin validation

--- src/containers/user/pin.jsx
+++ src/containers/user/pin.jsx
@@ -4,27 +4,72 @@ import i18n from "../../utils/i18n";
 // STYLE
 import style from "./style.css";
 
-class PIN extends React.Component {  
-    render() {  
-      return (
-        <div className={style.formPIN}>
-          <img src="../../images/logo.svg" className={style.logo} />
-          <div className={style.descriptionPIN}>{i18n.t("PIN_HEADER")}</div>
+// COMPONENTS
+import ModalBar from "../../components/modalBar";
+
+class PIN extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      pinInput: undefined,
+      modalError: false
+    }
+  }
   
-          <input type="password" className={style.inputPINDefault} maxLength="1"/>
-          <input type="password" className={style.inputPINDefault} maxLength="1"/>
-          <input type="password" className={style.inputPINDefault} maxLength="1"/>
-          <input type="password" className={style.inputPINDefault} maxLength="1"/>  
+  onInputChange = (inputValue) => {
 
-          <div className={style.descriptionLink}>{i18n.t("PIN_FORGET_PIN_LINK")}</div>
+    this.setState({
+      pinInput: inputValue
+    })
+   return this.setState({
+    modalError: false
+  })
+  }
 
-          <button className={style.buttonBorderGreen}>
+  inputValidator = () => {
+    let { userInput } = this.state
+    if (!userInput) {
+      return this.setState({
+        modalError: true
+      })
+    }   
+  }
+
+  showModalError = () => {
+    let { modalError } = this.state
+    if (modalError) {
+      return (
+        <ModalBar
+          type={"error"}
+          message={"Digite o código correto"}
+          timer
+        />
+      )}
+    return;
+  }
+
+
+  render() {
+    return (
+      <div className={style.formPIN}>
+      {this.showModalError()}
+        <img src="../../images/logo.svg" className={style.logo} />
+        <div className={style.descriptionPIN}>{i18n.t("PIN_HEADER")}</div>
+
+        <input type="password" className={style.inputPINDefault} maxLength="1"  onChange={(insert) => this.onInputChange(insert.target.value)}/>
+        <input type="password" className={style.inputPINDefault} maxLength="1"  onChange={(insert) => this.onInputChange(insert.target.value)}/>
+        <input type="password" className={style.inputPINDefault} maxLength="1"  onChange={(insert) => this.onInputChange(insert.target.value)}/>
+        <input type="password" className={style.inputPINDefault} maxLength="1"  onChange={(insert) => this.onInputChange(insert.target.value)}/>
+
+        <div className={style.descriptionLink}>{i18n.t("PIN_FORGET_PIN_LINK")}</div>
+
+        <button className={style.buttonBorderGreen} onClick={() => this.inputValidator()}>
           {i18n.t("BTN_LOGIN")}
         </button>
-        
-        </div>
-      );
-    }
+
+      </div>
+    );
+  }
 }
 
 export default PIN;
