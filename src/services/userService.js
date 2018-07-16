65799208d2dfbb3f2b2af1ef24ffe88455e71cf0
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-07-16 16:36:41 -0300

    passed new value on header of app

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-import { BASE_URL } from "../constants/apiBaseUrl";
+import { BASE_URL, API_KEY } from "../constants/apiBaseUrl";
 class UserService {
+
 
     async userAuthenticate(email, password) {
         try {
-            // console.warn("Leo env ",process.env.REACT_APP_ENV_APIKEY);
             let request = await axios.post(`${BASE_URL}/login`, {
                 login: email,
                 password
-            });
-        
+            }, API_KEY);
+            
             return request;
         } catch (error) {
             console.error(error);
