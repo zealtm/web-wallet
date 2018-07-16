d3fda77f4d16356eff4bf7e0af357bfdca9fee60
Guilherme Guercione <guilherme.vecino@lunes.io>
2018-07-16 11:18:21 -0300

    Init

--- src/utils/formValidator.js
+++ src/utils/formValidator.js
@@ -1,5 +1,5 @@
-import validator from 'validator';
-import isEmail from 'validator/lib/isEmail';
+// import validator from 'validator';
+// import isEmail from 'validator/lib/isEmail';
 
 export const FormValidator = (value) => {
   value += 1;
