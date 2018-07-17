2ac92505cc7b654222d37197230ec278daad10fa
Hellen <hellen.ferreira@lunes.io>
2018-07-17 18:09:38 -0300

    inputs validation

--- src/containers/user/reset/reset.jsx
+++ src/containers/user/reset/reset.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import i18n from "../../../utils/i18n";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { authenticate } from "../redux/userAction";
+import { clearMessage, errorInput } from "../../errors/redux/errorAction";
 
 // COMPONENTS
 import Footer from "../footer";
+
+// UTILS
+import i18n from "../../../utils/i18n";
+import { inputValidator } from "../../../utils/inputValidator";
 
 // STYLE
 import style from "../style.css";
@@ -14,8 +22,10 @@ class Reset extends React.Component {
     super()
     this.state = {
       step: 0,
-      userInput: undefined,
-      inputError: false
+      inputs: {
+        email: undefined
+      },
+      errors: undefined
     }
   }
 
@@ -23,24 +33,32 @@ class Reset extends React.Component {
     let { name, value } = input;
     this.setState({
       ...this.state,
-      inputs: { ...this.state.inputs, [name]: { type: name, value } }
+      inputs: { ...this.state.inputs, [name]: { type: name, value } },
+      errors: undefined
     });
   };
 
-
   inputValidator = () => {
-    let { step, userInput } = this.state
-    if (!userInput) {
-      return this.setState({
-        inputError: true
-      })
+    let { clearMessage, errorInput } = this.props
+    let { inputs, step } = this.state;
+    let { email } = inputs;
+    let { messageError, errors } = inputValidator(inputs);
+
+    if (errors.length > 0) {
+      errorInput(messageError);
+      this.setState({
+        ...this.state,
+        errors
+      });
     } else {
+      clearMessage();
+      authenticate(email.value);
       this.setState({ step: step + 1 });
     }
-  }
+  };
 
   cont_1 = () => {
-    let { inputError } = this.state;
+    let { errors } = this.state;
     return (
       <div>
         <img src="../../images/logo.svg" className={style.logo} />
@@ -52,9 +70,11 @@ class Reset extends React.Component {
         <div className={style.p2}>{i18n.t("RESET_INSTRUCTIONS2")}</div>
 
         <input
+          type="email"
+          name="email"
           placeholder={i18n.t("PLACEHOLDER_EMAIL")}
-          className={inputError ? style.inputError : style.inputTextDefault}
           onChange={(value) => this.getInput(value.target.value)}
+          className={errors && errors.includes('email') ? style.inputError : style.inputTextDefault}
         />
 
         <button className={style.buttonBorderGreen} onClick={() => this.inputValidator()}>
@@ -102,4 +122,24 @@ class Reset extends React.Component {
   }
 }
 
-export default Reset;
+
+Reset.propTypes = {
+  authenticate: PropTypes.func,
+  clearMessage: PropTypes.func,
+  errorInput: PropTypes.func
+};
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      authenticate,
+      clearMessage,
+      errorInput
+    },
+    dispatch
+  );
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Reset);
