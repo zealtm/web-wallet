6f47adc60f43564f172ac41d7aa0530591bb17a3
Leonardo Ribeiro <leo.ribeiro@lunes.io>
2018-08-07 15:12:10 -0300

    fixed resoluytion of screen

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -8,119 +8,35 @@ import Grid from "@material-ui/core/Grid";
 // STYLE
 import style from "./style.css";
 
-
 class Home extends React.Component {
+  createCards = () => {
+    let cards = [];
+    for (let index = 0; index < 6; index++) {
+      cards.push(
+        <Grid item>
+          <div className={style.cardHome}>
+            <img src="/images/icons/wallet/wallet@3x.png" className={style.cardIcon} />
+            <div className={style.cardTitle}>{i18n.t("CARD_HOME_TITLE_1")} </div>
+            <div className={style.descriptionCard}>
+              {i18n.t("CARD_HOME_DESCRIPTION_1")}
+            </div>
+
+            <Link to="#">
+              <button className={style.buttonCard}>
+                {i18n.t("BTN_LOGIN")}
+              </button>
+            </Link>
+          </div>
+        </Grid>)
+    }
+
+    return cards;
+  }
   render() {
     return (
-      <div>
+      <div >
         <Grid container className={style.homeContainer}>
-
-          <Grid item>
-            <div className={style.cardHome}>
-              <img src="/images/icons/wallet/wallet@3x.png" className={style.cardIcon} />
-              <div className={style.cardTitle}>{i18n.t("CARD_HOME_TITLE_1")} </div>
-              <div className={style.descriptionCard}>
-                {i18n.t("CARD_HOME_DESCRIPTION_1")}
-              </div>
-
-              <Link to="#">
-                <button className={style.buttonCard}>
-                  {i18n.t("BTN_LOGIN")}
-                </button>
-              </Link>
-
-            </div>
-          </Grid>
-
-
-          <Grid item>
-            <div className={style.cardHome}>
-              <img src="/images/icons/wallet/wallet@3x.png" className={style.cardIcon} />
-              <div className={style.cardTitle}>{i18n.t("CARD_HOME_TITLE_2")} </div>
-              <div className={style.descriptionCard}>
-                {i18n.t("CARD_HOME_DESCRIPTION_2")}
-              </div>
-
-              <Link to="#">
-                <button className={style.buttonCard}>
-                  {i18n.t("BTN_LOGIN")}
-                </button>
-              </Link>
-
-            </div>
-          </Grid>
-
-
-          <Grid item>
-            <div className={style.cardHome}>
-              <img src="/images/icons/wallet/wallet@3x.png" className={style.cardIcon} />
-              <div className={style.cardTitle}>{i18n.t("CARD_HOME_TITLE_3")} </div>
-              <div className={style.descriptionCard}>
-                {i18n.t("CARD_HOME_DESCRIPTION_3")}
-              </div>
-
-              <Link to="#">
-                <button className={style.buttonCard}>
-                  {i18n.t("BTN_LOGIN")}
-                </button>
-              </Link>
-
-            </div>
-          </Grid>
-
-
-          <Grid item>
-            <div className={style.cardHome}>
-              <img src="/images/icons/wallet/wallet@3x.png" className={style.cardIcon} />
-              <div className={style.cardTitle}>{i18n.t("CARD_HOME_TITLE_4")} </div>
-              <div className={style.descriptionCard}>
-                {i18n.t("CARD_HOME_DESCRIPTION_4")}
-              </div>
-
-              <Link to="#">
-                <button className={style.buttonCard}>
-                  {i18n.t("BTN_LOGIN")}
-                </button>
-              </Link>
-
-            </div>
-          </Grid>
-
-          <Grid item>
-            <div className={style.cardHome}>
-              <img src="/images/icons/wallet/wallet@3x.png" className={style.cardIcon} />
-              <div className={style.cardTitle}>{i18n.t("CARD_HOME_TITLE_5")} </div>
-              <div className={style.descriptionCard}>
-                {i18n.t("CARD_HOME_DESCRIPTION_5")}
-              </div>
-
-              <Link to="#">
-                <button className={style.buttonCard}>
-                  {i18n.t("BTN_LOGIN")}
-                </button>
-              </Link>
-
-            </div>
-          </Grid>
-
-          <Grid item>
-            <div className={style.cardHome}>
-              <img src="/images/icons/wallet/wallet@3x.png" className={style.cardIcon} />
-              <div className={style.cardTitle}>{i18n.t("CARD_HOME_TITLE_6")} </div>
-              <div className={style.descriptionCard}>
-                {i18n.t("CARD_HOME_DESCRIPTION_6")}
-              </div>
-
-              <Link to="#">
-                <button className={style.buttonCard}>
-                  {i18n.t("BTN_LOGIN")}
-                </button>
-              </Link>
-
-            </div>
-          </Grid>
-
-
+          {this.createCards().map(cards => <div key={Math.random()}>{cards}</div>)}
         </Grid>
       </div>
     );
