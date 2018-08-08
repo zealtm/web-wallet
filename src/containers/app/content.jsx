ee0a232d3561f5f8ccc615a385d71fbe6456b35e
Guilherme Guercione <guilherme.vecino@lunes.io>
2018-08-08 18:07:39 -0300

    Loading False

--- src/containers/app/content.jsx
+++ src/containers/app/content.jsx
@@ -30,9 +30,9 @@ class Content extends Component {
   }
 
   componentDidUpdate() {
-    console.warn('FOI')
     let { type } = this.state;
     let { loading } = this.props.skeleton;
+
     if (loading) {
       if (type !== "loading") return this.renderLoading();
       return;
