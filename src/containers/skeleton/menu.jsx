ac47267ac95154b3802a1fb8b11d2ceaca66467c
Guilherme Guercione <guilherme.vecino@lunes.io>
2018-08-08 12:28:36 -0300

    active menu ajusts

--- src/containers/skeleton/menu.jsx
+++ src/containers/skeleton/menu.jsx
@@ -1,72 +1,92 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, withRouter } from "react-router-dom";
 
-// MATERIAL UI 
+// MATERIAL UI
 import Grid from "@material-ui/core/Grid";
 import Hidden from "@material-ui/core/Hidden";
 import Avatar from "@material-ui/core/Avatar";
 
-// STYLE 
+// STYLE
 import style from "./style.css";
 
 // array itens menu
 const menuItens = [
-    {
-        link: "/home",
-        label: "Home",
-        icon: "../../images/icons/home/home@1x.png",
-    },
-    {
-        link: "/wallet",
-        label: "Wallet",
-        icon: "../../images/icons/wallet/wallet@1x.png",
-    },
-    {
-        link: "/boleto",
-        label: "Boletos",
-        icon: "../../images/icons/home/home@1x.png",
-    },
+  {
+    link: "/home",
+    label: "Home",
+    icon: "../../images/icons/home/home@1x.png"
+  },
+  {
+    link: "/wallet",
+    label: "Wallet",
+    icon: "../../images/icons/wallet/wallet@1x.png"
+  },
+  {
+    link: "/boleto",
+    label: "Boletos",
+    icon: "../../images/icons/home/home@1x.png"
+  }
 ];
 
 class Menu extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    renderMenu = () => {
-        return menuItens.map((item,key) => {
-            return (
-                <NavLink className={style.linkMenu} activeClassName={style.linkMenuActive} to={item.link} key={key}>
-                    <img src={item.icon} className={style.iconMenu} />{item.label}
-                </NavLink>
-            )
-        });
-    }
+  renderMenu = () => {
+    let { pathname } = this.props.location;
+    
+    return menuItens.map((item, key) => {
+      let classStyle = style.linkMenu;
 
-    render() {
-        const { openMenu } = this.props;
-        return (
-            <div className={style.colMenu} style={{left: + (openMenu) ? ' 0px ' : '-232px'}}>
-                <Hidden lgUp>
-                    <Grid container className={style.boxUserMenu}>
-                        <Grid item xs={4} align="center">
-                            <Avatar alt="Avatar" src="https://loremflickr.com/80/80" />
-                        </Grid>
-                        <Grid item xs={8}>
-                            Nome Usuario<br />
-                            <Link to="/logout" className={style.link}>Logout</Link>
-                        </Grid>
-                    </Grid>
-                </Hidden>
-                {this.renderMenu()}
-            </div>
-        )
-    }
+      if(item.label === "Home" && (pathname === "/" || pathname === "/home")) {
+        classStyle = style.linkMenu + " " + style.linkMenuActive
+      }
+
+      return (
+        <NavLink
+          className={classStyle}
+          activeClassName={style.linkMenuActive}
+          to={item.link}
+          key={key}
+        >
+          <img src={item.icon} className={style.iconMenu} />
+          {item.label}
+        </NavLink>
+      );
+    });
+  };
+
+  render() {
+    const { openMenu } = this.props;
+    return (
+      <div
+        className={style.colMenu}
+        style={{ left: +openMenu ? " 0px " : "-232px" }}
+      >
+        <Hidden lgUp>
+          <Grid container className={style.boxUserMenu}>
+            <Grid item xs={4} align="center">
+              <Avatar alt="Avatar" src="https://loremflickr.com/80/80" />
+            </Grid>
+            <Grid item xs={8}>
+              Nome Usuario<br />
+              <Link to="/logout" className={style.link}>
+                Logout
+              </Link>
+            </Grid>
+          </Grid>
+        </Hidden>
+        {this.renderMenu()}
+      </div>
+    );
+  }
 }
 
 Menu.propTypes = {
-    openMenu: PropTypes.bool.isRequired
+  openMenu: PropTypes.bool.isRequired,
+  location: PropTypes.object.isRequired
 };
 
-export default Menu;
+export default withRouter(Menu);
