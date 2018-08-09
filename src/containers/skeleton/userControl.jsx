0901426627d9cf722650c32317c00dd100e898da
Thiago Pereira <thiago_salper@hotmail.com>
2018-08-09 10:18:29 -0300

    ajust popup menu user

--- src/containers/skeleton/userControl.jsx
+++ src/containers/skeleton/userControl.jsx
@@ -1,58 +1,62 @@
 import React from "react";
-import {Link} from "react-dom";
-import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 //MATERIAL UI 
-import {withStyles} from "@material-ui/core/styles";
 import Avatar from '@material-ui/core/Avatar';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 
 //STYLE
 import style from "./style.css";
-
-const classes = {
-  muiMenu: {
-    marginTop:48, 
-  }, 
-  boxHeaderUser: { 
-    fontSize:14, 
-    minWidth:160, 
-    padding:16
-  }, 
-  linkMenu: {
-    fontSize:11,
-    paddingBottom:5,
-    paddingTop:5
-  }, 
-  dividerMenu: {
-    borderBottom:'solid 1px #ccc',
-    borderTop:'none',
-    margin:0
-  }
-};
-
 
 class UserControl extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      openBox: null, 
+      openBox: false, 
       avatar: 'https://loremflickr.com/80/80',
     }
   }
 
-  handleClick = event => {
-    this.setState({...this.state, openBox: event.currentTarget });
+  handleClick = () => {
+    this.setState({...this.state, openBox: !this.state.openBox});
   };
 
-  handleClose = () => {
-    this.setState({...this.state, openBox: null });
-  };
+  renderPopup = () => {
+    const {openBox} = this.state;
+    
+    if(openBox){
+
+      setTimeout(()=>{
+        this.setState({...this.state, openBox: false})
+      }, 5000);
+
+      return (
+        <div className={style.menuUser}>
+          <div className={style.arrowUp}></div>
+          <Link to="/" className={style.linkPopMenu}>
+            <div className={style.boxIcon}>
+            <img src="../../images/icons/settings/settings.png" />
+            </div>
+            Configurações
+          </Link>
+          <Link to="/" className={style.linkPopMenu}>
+            <div className={style.boxIcon}>
+            <img src="../../images/icons/question/question.png" />
+            </div>
+            Ajuda
+          </Link>
+          <Link to="/" className={style.linkPopMenu}>
+            <div className={style.boxIcon}>
+            <img src="../../images/icons/exit/exit.png" />
+            </div>
+            Sair
+          </Link>
+        </div>
+      );
+    }
+  }
 
   render(){
-    const {avatar, openBox} = this.state;
-    const {classes} = this.props;
+    const {avatar} = this.state;
 
     return (
       <div>
@@ -60,45 +64,13 @@ class UserControl extends React.Component {
           alt="Avatar" 
           src={avatar} 
           className={style.avatarHeader}
-          aria-controls="menuUser" 
           onClick={this.handleClick} />
 
-          {/* <div style={
-            {
-              backgroundColor: '#fff',
-              padding:16,
-              dropShadow: '10px 10px 10px rgba(0,0,0,.3)',
-              position: 'absolute',
-              width: 200,
-              top:64,
-              right:16,
-            }
-          }>
-          asdasdasdasdasdasdasd
-          </div> */}
+          {this.renderPopup()}
 
-          <Menu
-            id="menuUser"
-            className={classes.muiMenu}
-            anchorEl={openBox}
-            open={Boolean(openBox)}
-            onClose={this.handleClose} >
-              <div className={classes.boxHeaderUser}>
-                Olá, Thiago
-              </div>
-              <MenuItem onClick={this.handleClose} className={classes.linkMenu}>Privacidade</MenuItem>
-              <MenuItem onClick={this.handleClose} className={classes.linkMenu}>Configuração</MenuItem>
-              <MenuItem onClick={this.handleClose} className={classes.linkMenu}>Suporte</MenuItem>
-              <hr className={classes.dividerMenu} />
-              <MenuItem onClick={this.handleClose} className={classes.linkMenu}>Sair</MenuItem>
-          </Menu>
       </div>
     );
   }
 }
 
-UserControl.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(classes)(UserControl);
+export default UserControl;
