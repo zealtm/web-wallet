27de0271c867f360206b9dcb14da08c486141cfd
Hellen <hellen.ferreira@lunes.io>
2018-08-14 12:34:06 -0300

    Buttons - receive/action - Init.

--- src/containers/wallet/index.jsx
+++ src/containers/wallet/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
+// UTILS
+import i18n from "../../utils/i18n";
+
+// STYLE
+import style from "./style.css";
+
 class Wallet extends React.Component {
   render() {
-    return <div>Wallet</div>;
+    return <div>
+      Wallet
+      <div>
+        <button className={style.receiveSubmit}>  {i18n.t("BTN_RECEIVE")} </button>
+        <button className={style.receiveButton}>  {i18n.t("BTN_SUBMIT")} </button>
+      </div>
+
+    </div>;
   }
 }
 
