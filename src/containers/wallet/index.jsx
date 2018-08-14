40a99fa72b89ffde1277b09ac07b01b9bb4a5faf
Hellen <hellen.ferreira@lunes.io>
2018-08-14 16:38:45 -0300

    Screen wallet (route) - Init.

--- src/containers/wallet/index.jsx
+++ src/containers/wallet/index.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 
+// STYLE
+import style from "./style.css";
+
+// MATERIAL UI
+import Grid from "@material-ui/core/Grid";
+
 class Wallet extends React.Component {
   render() {
-    return <div>Wallet</div>;
+    return (
+      <div className={style.containerWallet}>
+        <Grid xs={8} lg={8} xl={12} className={style.contInfoCoins}>
+          <div className={style.coinSelected}>
+            coin
+            </div>
+
+            <div className={style.coinBalance}>
+            balance dddddddddddddddddd
+            <br/>
+            dddddd
+            </div>
+        </Grid>
+      </div>
+    );
   }
 }
 
