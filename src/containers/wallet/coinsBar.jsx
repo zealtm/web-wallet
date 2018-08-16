c64da697bab78185bca9f1a3a63ed10fe0b90e36
Guilherme Guercione <guilherme.vecino@lunes.io>
2018-08-16 16:10:06 -0300

    Dinamic Coins

--- src/containers/wallet/coinsBar.jsx
+++ src/containers/wallet/coinsBar.jsx
@@ -7,6 +7,10 @@ import { connect } from "react-redux";
 // import { bindActionCreators } from "redux";
 // import { clearMessage, errorInput } from "../../errors/redux/errorAction";
 
+// UTILS
+import { percentCalc } from "../../utils/numbers";
+import { getDefaultFiat } from "../../utils/localStorage";
+
 // MATERIAL UI
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
@@ -20,40 +24,6 @@ import ArrowDropUp from "@material-ui/icons/ArrowDropUp";
 
 // STYLE
 import style from "./style.css";
-
-// DATA EXEMPLO
-const coinse = [
-  {
-    name: "LUNES",
-    balance: "$0.005",
-    percent: "-1.55%"
-  },
-  {
-    name: "BTC",
-    balance: "$8,000.30",
-    percent: "8.00%"
-  },
-  {
-    name: "LTC",
-    balance: "$200.00",
-    percent: "5.25%"
-  },
-  {
-    name: "USDT",
-    balance: "$200.00",
-    percent: "5.25%"
-  },
-  {
-    name: "DASH",
-    balance: "$200.00",
-    percent: "5.25%"
-  },
-  {
-    name: "ETH",
-    balance: "$200.00",
-    percent: "5.25%"
-  }
-];
 
 class CoinsBar extends React.Component {
   constructor(props) {
@@ -74,8 +44,7 @@ class CoinsBar extends React.Component {
   };
 
   renderArrowPercent = val => {
-    const percent = parseFloat(val);
-    if (percent < 0) {
+    if (val < 0) {
       return <ArrowDropDown className={style.arrowPercentDown} />;
     } else {
       return <ArrowDropUp className={style.arrowPercentUp} />;
@@ -84,62 +53,56 @@ class CoinsBar extends React.Component {
 
   renderCoins = () => {
     let { coins } = this.props.skeleton;
-    let teste = [ lunes: { name: "lunes" } , btc: { name: "btc" } ];
-    console.warn("renderCoins", coins);
-    console.warn("renderCoins T", teste);
+    let defaultFiat = getDefaultFiat();
+    console.warn(coins)
+    return Object.keys(coins).map((val, index) => {
+      let coin = coins[val];
+      let coinBalance = coins[val].balance.available;
+      let coinFiatBalance = (
+        coinBalance * coins[val].price[defaultFiat].price
+      ).toFixed(2);
+      let coinPercent = percentCalc(coins[val].price[defaultFiat].price, 100);
 
-    return teste.map(coin => {
-      console.warn("coin", coin);
-
-      return;
+      return (
+        <div
+          className={style.baseBoxCoin}
+          key={index}
+          onClick={() => this.selectCoin(coin.abbreviation)}
+        >
+          <div
+            className={
+              this.state.coinActive === coins.abbreviation
+                ? style.boxCoinActive
+                : style.boxCoin
+            }
+          >
+            <div className={style.boxIconCoin}>
+              <img
+                className={style.iconCoin}
+                src={`images/coins/${coin.abbreviation}.png`}
+              />
+            </div>
+            <Hidden smDown>
+              <div className={style.boxLabelCoin}>
+                {"$" + coinFiatBalance} <br />
+                <div className={style.labelPercent}>
+                  {this.renderArrowPercent(coinPercent)}
+                  {coinPercent + "%"}
+                </div>
+              </div>
+            </Hidden>
+            <Hidden mdUp>
+              <div className={style.boxArrowPercent}>
+                {this.renderArrowPercent(coinPercent)}
+              </div>
+            </Hidden>
+          </div>
+        </div>
+      );
     });
-
-    // return coins.map((val, index) => {
-    //   console.warn('map', val)
-    //   return (
-    //     <div
-    //       className={style.baseBoxCoin}
-    //       key={index}
-    //       onClick={() => this.selectCoin(coins.abbreviation)}
-    //     >
-    //       <div
-    //         className={
-    //           this.state.coinActive === coins.abbreviation
-    //             ? style.boxCoinActive
-    //             : style.boxCoin
-    //         }
-    //       >
-    //         <div className={style.boxIconCoin}>
-    //           <img
-    //             className={style.iconCoin}
-    //             src={`images/coins/${coins.abbreviation}.png`}
-    //           />
-    //         </div>
-    //         <Hidden smDown>
-    //           <div className={style.boxLabelCoin}>
-    //             {val.balance} <br />
-    //             <div className={style.labelPercent}>
-    //               {this.renderArrowPercent(val.percent)}
-    //               {val.percent}
-    //             </div>
-    //           </div>
-    //         </Hidden>
-    //         <Hidden mdUp>
-    //           <div className={style.boxArrowPercent}>
-    //             {this.renderArrowPercent(val.percent)}
-    //           </div>
-    //         </Hidden>
-    //       </div>
-    //     </div>
-    //   );
-    // });
   };
 
   render() {
-    console.warn("render");
-    let { coins } = this.props.skeleton;
-    console.warn("coins after", coins);
-
     let settings = {
       arrows: false,
       draggable: true,
