c59cab130da816007eba8d5f20688a1cc05cbcb0
Guilherme Guercione <guilherme.vecino@lunes.io>
2018-08-17 18:27:34 -0300

    loading

--- src/containers/wallet/index.jsx
+++ src/containers/wallet/index.jsx
@@ -1,18 +1,58 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+// REDUX
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { setWalletLoading } from "./redux/walletAction";
 
 // COMPONENTS
 import CoinsBar from "./coinsBar";
 import CoinsInfo from "./coinsInfo";
 
 class Wallet extends React.Component {
-  render() {
+  renderContent = () => {
+    let { loading } = this.props.wallet;
+    // console.warn("loading", loading);
+    // let { setWalletLoading } = this.props;
+
+    // setWalletLoading(true);
+
+    if (loading) {
+      return <div>Carregando...</div>;
+    }
+
     return (
       <div>
         <CoinsBar />
         <CoinsInfo />
       </div>
     );
+  };
+
+  render() {
+    return this.renderContent();
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  skeleton: PropTypes.object,
+  setWalletLoading: PropTypes.func
+};
+
+const mapSateToProps = store => ({
+  wallet: store.wallet
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      setWalletLoading
+    },
+    dispatch
+  );
+
+export default connect(
+  mapSateToProps,
+  mapDispatchToProps
+)(Wallet);
--- src/containers/wallet/redux/walletAction.js
+++ src/containers/wallet/redux/walletAction.js
@@ -2,3 +2,8 @@ export const setSelectedCoin = coin => ({
   type: "SET_SELECTED_COIN",
   coin
 });
+
+export const setWalletLoading = state => ({
+  type: "SET_WALLET_LOADING",
+  state
+});
