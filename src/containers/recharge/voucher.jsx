bdd9d92bfd97741e61e0997f6b084291bfc44c71
davidalves1 <alves.david@outlook.com>
2018-08-23 17:35:19 -0300

    Validate voucher fields (WIP)

--- src/containers/recharge/voucher.jsx
+++ src/containers/recharge/voucher.jsx
@@ -1,23 +1,30 @@
 import React from "react"
 import i18n from "../../utils/i18n";
 
-import { Grid, Input, InputLabel, FormControl } from "@material-ui/core";
+import { Grid, Input } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
-
 import style from "./style.css";
 import colors from "../../components/bases/colors";
+
+import Code from './code'
+
+import { inputValidator } from "../../utils/inputValidator";
 
 const inputStyle = {
   root: {
     color: colors.messages.info,
     margin: "1rem 0",
-    fontFamily: "Noto Sans, sans-serif",
-    color: colors.messages.info,
-    fontSize: "17px",
-    letterSpacing: "0.5px",
+    padding: '5px',
+    width: 'calc(100% - 20px)',
     "&:hover:before": {
       borderBottomColor: colors.purple.dark,
     },
+  },
+  cssInput: {
+    fontFamily: "Noto Sans, sans-serif",
+    fontSize: "17px",
+    letterSpacing: "0.5px",
+    textAlign: 'center',
   },
   cssUnderline: {
     "&:before, &:after": {
@@ -26,10 +33,10 @@ const inputStyle = {
     "&:hover:not(&$disabled):not(&$error):not(&$focused):before": {
       borderBottomColor: colors.purple.dark,
     },
-    disabled: {},
-    error: {},
-    focused: {},
   },
+  marginGrid: {
+    marginTop: '2rem'
+  }
 }
 
 class Voucher extends React.Component {
@@ -37,37 +44,65 @@ class Voucher extends React.Component {
     super();
 
     this.state = {
-      phone: '',
-      code0: '',
-      code1: '',
-      code2: '',
-      code3: '',
+      phone: ['',''],
+      code: ['','','',''],
+      errors: undefined
     }
+
+    this.handleCodeChange = this.handleCodeChange.bind(this);
   }
 
-  handleChange = name => event => {
-    console.log(name, event.target.value);
+  handlePhoneChange = (id) => event => {
+    const phone = this.state.phone;
+    phone[id] = event.target.value;
+
     this.setState({
       ...this.state,
-      [name]: event.target.value
+      phone
     });
   }
 
-  renderCodeInputs = () => {
-    return (
-      [...Array(4).keys()].map((id) => {
-        return (
-          <Grid key={id} item xs={6} sm={3}>
-            <input key={id} className={style.inputTextDefault} placeholder="1234" onChange={this.handleChange(`code${id}`)}/>
-          </Grid>
-        );
-      })
-    )
+  handleCodeChange = (id, value) => {
+    const code = this.state.code;
+    code[id] = value;
+
+    this.setState({
+      ...this.state,
+      code
+    });
+  }
+
+  inputValidator = () => {
+    const {phone, code} = this.state;
+
+    const phoneInput = {
+      type: "text",
+      name: "phone",
+      placeholder: "Phone",
+      value: phone.join('').replace(/\D/, ''),
+      required: true,
+      minLength: 11,
+      maxLength: 11,
+    }
+
+    const codeInput = {
+      type: "text",
+      name: "code",
+      placeholder: "Code",
+      value: code.join('').replace(/\D/, ''),
+      required: true,
+      minLength: 16,
+      maxLength: 16,
+    }
+
+    let { messageError, errors } = inputValidator({phone: phoneInput, code: codeInput});
+
+    console.log('errorMessage', messageError, 'errors', errors);
   }
 
   render() {
     const { classes } = this.props;
-    const { phone } = this.state;
+    const { phone, code } = this.state;
 
     return (
       <Grid container>
@@ -76,29 +111,39 @@ class Voucher extends React.Component {
         <Grid item xs={12} sm={6}>
           <div className={style.wrap}>
             <label className={style.inputLabel} htmlFor="txtPhone">{i18n.t("VOUCHER_NUMBER")}</label>
-            <div className="wrapInput">
-              <Input id="txtPhone"
-                classes={{ root: classes.root, underline: classes.cssUnderline }}
-                className={style.defaultText}
-                placeholder="(xx) xxxxx-xxxx"
-                value={phone}
-                onChange={this.handleChange('phone')}
-                fullWidth
-              />
-            </div>
-          </div>
-
-          <div className={style.wrap}>
-            <label>{i18n.t("VOUCHER_CODE")}</label>
             <Grid container>
-              {this.renderCodeInputs()}
+              <Grid item xs={4}>
+                <Input
+                  classes={{ root: classes.root, underline: classes.cssUnderline, input: classes.cssInput }}
+                  placeholder="(xx)"
+                  value={phone[0]}
+                  inputProps={{maxLength: 2, required: true}}
+                  onChange={this.handlePhoneChange(0)}
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <Input
+                  classes={{ root: classes.root, underline: classes.cssUnderline, input: classes.cssInput }}
+                  placeholder="xxxxx-xxxx"
+                  value={phone[1]}
+                  inputProps={{maxLength: 9, required: true}}
+                  onChange={this.handlePhoneChange(1)}
+                />
+              </Grid>
             </Grid>
           </div>
 
           <div className={style.wrap}>
+            <label className={style.marginLabel}>{i18n.t("VOUCHER_CODE")}</label>
+            <Code values={code} onHandleChange={this.handleCodeChange} />
+          </div>
+
+          <div className={style.wrap}></div>
+
+          <div className={style.wrap}>
             <button
               className={style.buttonBorderGreen}
-              onClick={() => console.log(this.state)}
+              onClick={() => this.inputValidator()}
             >
               {i18n.t("VOUCHER_BUTTON")}
             </button>
