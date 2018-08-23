9ce28b8172462fc2890341761d915dfe806988f0
Guilherme Guercione <guilherme.vecino@lunes.io>
2018-08-23 14:06:23 -0300

    Header Test

--- src/constants/headers.js
+++ src/constants/headers.js
@@ -1,7 +1,8 @@
 export const HEADER_RESPONSE = "x-amzn-remapped-authorization";
 export const HEADER_REQUEST = {
   headers: {
-    "access-control-allow-headers": "*",
-    "access-control-allow-origin": "*"
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept"
   }
 };
