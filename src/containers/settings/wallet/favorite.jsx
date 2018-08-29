c513df586f3dd70724500ad07d067b8532e4da10
Thiago Pereira <thiago_salper@hotmail.com>
2018-08-29 16:21:08 -0300

    config style done

--- src/containers/settings/wallet/favorite.jsx
+++ src/containers/settings/wallet/favorite.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 
+// STYLES
+import style from "./style.css";
+
+// COMPONENTS 
+import WalletRow from "./walletRow";
+
+const wallets = [
+  {
+    name: "LUNES",
+    address: "",
+    favorite: true,
+  },
+  {
+    name: "LUNES",
+    address: "",
+    favorite: false,
+  },
+  {
+    name: "LUNES",
+    address: "",
+    favorite: true,
+  }
+];
 
 class FavoritePage extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  renderWallets(list){
+    return list.map((val,key)=>{
+      return <div key={key}>
+        <WalletRow coin={val} />
+      </div>
+    });
+  }
+
   render(){
     return (
-      <div>Favorite</div>
+      <div className={style.box}>
+        {this.renderWallets(wallets)}
+      </div>
     )
   }
 }
