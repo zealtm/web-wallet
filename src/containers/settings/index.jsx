244a9562249da16c3b938e4eaa246883e0d99d74
Leonardo <leonardostudent94@hotmail.com>
2018-09-03 17:39:30 -0300

    margin style

--- src/containers/settings/index.jsx
+++ src/containers/settings/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import HomeSettings from "./homeSettings";
+import Security from "./security/security";
 
 class Configuration extends React.Component {
     render() {
         return (
             <div>
-                <HomeSettings/>
+                <Security/>
             </div>
         );
     }
