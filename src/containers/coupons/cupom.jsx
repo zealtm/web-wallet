b7e88b7f4355e7b1a727e533d1236fc02c95d99d
Marcelo <marcelorafaeldebrito@gmail.com>
2018-09-18 14:26:44 -0300

    Added redux to the coupon(Cupom component), and added some logics to validate what the user type in

--- src/containers/coupons/cupom.jsx
+++ src/containers/coupons/cupom.jsx
@@ -3,6 +3,16 @@ import i18n from "../../utils/i18n";
 import { Grid } from "@material-ui/core";
 import style from "./style.css";
 import Instructions from "./instructions";
+import PropTypes from "prop-types";
+
+// REDUX
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { errorRequest } from "../errors/redux/errorAction.js";
+import { verifyCoupon } from "./redux/couponsAction.js";
+
+//COMPONENTS
+import Loading from "../../components/loading.jsx";
 
 class Cupom extends React.Component {
   constructor() {
@@ -15,14 +25,37 @@ class Cupom extends React.Component {
   }
 
   handleChange = event => {
+    let val = event.target.value;
+    if (val.search(/[^0-9-]/g) !== -1) return;
     this.setState({
       ...this.state,
-      code: event.target.value
+      code: val
     });
   };
+  handleKeyDown = (event) => {
+    let { code } = this.state;
+    let key = event.keyCode;
+    let index = code.search(/([0-9]{4})(?!-)/g);
+    if (key !== 8 && index !== -1) {
+      let hyphens = code.match(/(-+)/g);
+      if (hyphens === null || hyphens.length !== 3)
+        code += '-';
+    }
+    this.setState({ code })
+  }
+  handleSend = () => {
+    const { code } = this.state;
+    if (code && code.length !== 19) {
+      errorRequest(i18n.t("SHORT_COUPON_LENGTH"));
+      return;
+    }
+    this.props.verifyCoupon(code);
+  }
 
   render() {
     const { code } = this.state;
+    const { coupons } = this.props;
+    const { coupon } = coupons;
 
     return (
       <Grid container direction="row" justify="center">
@@ -33,8 +66,9 @@ class Cupom extends React.Component {
               className={style.inputTextDefault}
               value={code}
               placeholder="1234-1234-1234-1234"
-              maxLength={16}
+              maxLength={19}
               onChange={this.handleChange}
+              onKeyDown={this.handleKeyDown}
               required
             />
           </div>
@@ -46,9 +80,9 @@ class Cupom extends React.Component {
           <div className={style.row}>
             <button
               className={style.buttonBorderGreen}
-              onClick={() => this.inputValidator()}
+              onClick={this.handleSend}
             >
-              {i18n.t("VOUCHER_BUTTON")}
+              {coupon.loading ? <Loading/> : i18n.t("VOUCHER_BUTTON")}
             </button>
           </div>
         </Grid>
@@ -57,4 +91,18 @@ class Cupom extends React.Component {
   }
 }
 
-export default Cupom;
+Cupom.propTypes = {
+  verifyCoupon: PropTypes.func.isRequired,
+  errorRequest: PropTypes.func,
+  coupons: PropTypes.object
+}
+const mapStateToProps = state => ({
+  coupons: state.coupons
+})
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({
+    errorRequest,
+    verifyCoupon
+  }, dispatch)
+
+export default connect(mapStateToProps,mapDispatchToProps)(Cupom);
