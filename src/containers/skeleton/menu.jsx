ca21f90602ba3fce7d9fd4ed3d70203d64498d5f
Guilherme Guercione <guivecino@gmail.com>
2018-09-18 17:34:17 -0300

    Test coins services

--- src/containers/skeleton/menu.jsx
+++ src/containers/skeleton/menu.jsx
@@ -34,16 +34,12 @@ const menuItens = [
     label: i18n.t("MENU_LEASING"),
     icon: "../../images/icons/general/leasing@1x.png"
   },
-  // {
-  //   link: "/assets",
-  //   label: i18n.t("MENU_ASSETS"),
-  //   icon: "../../images/icons/general/wallet@1x.png"
-  // },
-  // {
-  //   link: "/recharge",
-  //   label: i18n.t("MENU_RECHARGE"),
-  //   icon: "../../images/icons/general/recharge@1x.png"
-  // },
+  {
+    link: "/assets",
+    label: i18n.t("MENU_ASSETS"),
+    icon: "../../images/icons/general/wallet@1x.png"
+  },
+
   {
     link: "/payment",
     label: i18n.t("MENU_PAY"),
