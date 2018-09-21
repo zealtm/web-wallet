93a0ab6ee35fc49e8d0066ad6771129e9ff5186f
davidalves1 <alves.david@outlook.com>
2018-09-20 22:39:44 -0300

    Fix recharge errors

--- src/containers/recharge/modal/doneRecharge.jsx
+++ src/containers/recharge/modal/doneRecharge.jsx
@@ -3,31 +3,50 @@ import { connect } from "react-redux";
 import i18n from "../../../utils/i18n";
 import style from "./style.css";
 
+// COMPONENTS
+import Loading from "../../../components/loading";
+
+
 class DoneRecharge extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return (
-      <div className={style.modalBox}>
-        <img
-          src="/images/icons/confirm/confirm.png"
-          className={style.imageResult}
-        />
-        <div>
-          {i18n.t("RECHARGE_SUCCESS_1")}
-          <span className={style.textGreen}>R$ 30,00</span>
+    const {loading} = this.props;
+    if (loading) {
+      return (
+        <div className={style.modalBox}>
+          <Loading color="lunes" />
         </div>
+      )
+    } else {
+      return (
+        <div className={style.modalBox}>
+          <img
+            src="/images/icons/confirm/confirm.png"
+            className={style.imageResult}
+          />
+          <div>
+            {i18n.t("RECHARGE_SUCCESS_1")}
+            {i18n.t("RECHARGE_SUCCESS_2")}
+          </div>
 
-        <div className={style.smallDescription}>
-          {i18n.t("RECHARGE_SUCCESS_2")}
+          <div className={style.smallDescription}>
+            {i18n.t("PAYMENT_TEXT_HISTORY")}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
-const mapStateToProps = store => ({});
+DoneRecharge.propTypes = {
+  loading: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = store => ({
+  loading: store.payment.loading,
+});
 
 export default connect(mapStateToProps)(DoneRecharge);
