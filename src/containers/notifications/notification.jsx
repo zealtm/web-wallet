e89a4886019052ab10ac087ad5baf26df34364e5
Gabriel Sandes <gabriel.sandes@lunes.io>
2018-09-28 18:25:57 -0300

    Notification page

--- src/containers/notifications/notification.jsx
+++ src/containers/notifications/notification.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// STYLE
-import { Grid } from "@material-ui/core";
-import style from "./style.css";
+//UTILS
+import i18n from "../../utils/i18n";
 
+// MATERIAL UI
+import Grid from "@material-ui/core/Grid";
+
+// STYLE
+import style from "./style.css";
 class Notification extends React.Component {
   constructor() {
     super();
@@ -13,14 +17,25 @@ class Notification extends React.Component {
   render() {
     return (
       <Grid container direction="row" justify="center">
-        <h1>Notification</h1>
+        <h1>{i18n.t("NOTIFICATION_NUMBER_OF_NOTIFICATION")}</h1>
+        <div className={style.hr} />
+        <div>
+          <img
+            src="images/lunio/lunio-error@1x.gif"
+            className={style.imgNoNotification}
+          />
+          <div className={style.paragraphs}>
+            <p>
+              {i18n.t("NOTIFICATION_MESSAGE_1")} <br />{" "}
+              {i18n.t("NOTIFICATION_MESSAGE_2")}
+            </p>
+          </div>
+        </div>
       </Grid>
     );
   }
 }
 
-Notification.propTypes = {
-
-}
+Notification.propTypes = {};
 
 export default Notification;
