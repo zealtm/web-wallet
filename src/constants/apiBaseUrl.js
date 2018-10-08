832c1eeae3f26b5f4c19b783d38cce646426de51
Guilherme Guercione <guilherme.vecino@gmail.com>
2018-10-08 09:42:06 -0300

    TESTNET Var validation

--- src/constants/apiBaseUrl.js
+++ src/constants/apiBaseUrl.js
@@ -1,8 +1,8 @@
-export const BASE_URL = "https://a.lunes.io/wallet/v1";
-export const LUNESNODE_URL = "https://lunesnode.lunes.io";
+export const TESTNET = false;
+export const BASE_URL = TESTNET ? "https://a.lunes.io/wallet/staging" : "https://a.lunes.io/wallet/v1";
+export const LUNESNODE_URL = TESTNET ? "https://lunesnode-testnet.lunes.io" : "https://lunesnode.lunes.io";
 export const TETHER_URL = "https://api.omniwallet.org";
 export const HEADER_RESPONSE = "x-amzn-remapped-authorization";
-export const TESTNET = false;
 export const HEADER_REQUEST = {
   headers: {
     "Content-Type": "application/json",
