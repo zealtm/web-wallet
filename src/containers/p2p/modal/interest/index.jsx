afe72ca0127fda2948e59bbf25fb7e47facde458
adnet0910 <adelsosndcristo@hotmail.com>
2018-10-08 19:34:32 -0300

    Modal interesting, layout style

--- src/containers/p2p/modal/interest/index.jsx
+++ src/containers/p2p/modal/interest/index.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
-
 import style from "./style.css";
 
 class InterestModal extends React.Component {
   render(){
-    return (
-      <div className={style.modelo}>
-        <img src="" className={style.modelo} />
-        <div className={style.modelo}>
-          Um Player se interessou pela sua oferta e quer abrir um chat com você!
+    return <div className={style.modalContainerChat}>
+        <img src="/images/modal/chat@2x.png" className={style.modalImgChat} />
+        <div className={style.openChat}>
+        <p className={style.textOpenChat}>
+            Um Player se interessou pela sua oferta e quer abrir um chat com
+            você!
+          </p>
         </div>
-        <div className={style.modelo}>
-          <button className={style.modelo}>Conversar</button>
-          <button className={style.modelo}>Agora não</button>
+        <div className={style.btnChat}>
+          <button className={style.btnTalkChat}>Conversar</button>
+          <button className={style.btnNotTalkChat}>Agora não</button>
         </div>
       </div>
-    )
+      
   }
 }
 
