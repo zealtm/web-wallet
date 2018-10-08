a0202c7474e699be8419c05fe57c508fff83d873
Thiago Pereira <thiago_salper@hotmail.com>
2018-10-08 17:13:46 -0300

    style form

--- src/containers/p2p/createOffer/index.jsx
+++ src/containers/p2p/createOffer/index.jsx
@@ -5,6 +5,9 @@ import PropTypes from "prop-types";
 import { Grid, Avatar } from "@material-ui/core/";
 import { ArrowForward, Star } from "@material-ui/icons/";
 
+// COMPONENTS 
+import Select from "../../../components/select";
+
 // STYLE
 import style from "./style.css";
 
@@ -12,46 +15,122 @@ class CreateOffer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      title: "",
+      img: "",
+      coinsExample: "",
     };
   }
 
+  coinSelected = (value, title, img = undefined) => {
+    this.setState({
+      ...this.state,
+      coin: {
+        name: title,
+        value,
+        img
+      }
+    });
+  };
+
   render() {
+    const {title,img,coinsExample} = this.state;
     return (
       <div className={style.baseUser} >
-        <Grid container>
-          <Grid item xs={2}>
-            <Avatar
-              alt="avatar"
-              src="https://loremflickr.com/40/40"
-              className={style.avatar}
-            />
-          </Grid>
-          <Grid item xs={5}>
-            <span className={style.name}>Nome Usuario</span>
-            <span className={style.textSmall}>00/00/2018</span>
-           
-          </Grid>
-          <Grid item xs={5} style={{ paddingLeft: 10 }}>
-            <div className={style.boxStar}>
-              <Star className={style.starActive} />
-              <Star className={style.starActive} />
-              <Star className={style.starActive} />
-              <Star className={style.starActive} />
-              <Star className={style.star} />
+      
+          <div className={style.headerUser}>
+            <Grid container>
+              <Grid item xs={2}>
+                <Avatar
+                  alt="avatar"
+                  src="https://loremflickr.com/40/40"
+                  className={style.avatar}
+                />
+              </Grid>
+              <Grid item xs={5}>
+                <span className={style.name}>Nome Usuario</span>
+                <span className={style.textSmall}>00/00/2018</span>
+              </Grid>
+              <Grid item xs={5} style={{ paddingLeft: 10 }}>
+              <div className={style.boxStar}>
+                <Star className={style.starActive} />
+                <Star className={style.starActive} />
+                <Star className={style.starActive} />
+                <Star className={style.starActive} />
+                <Star className={style.star} />
+              </div>
+            </Grid>
+            </Grid>
+          </div>
+
+          <div className={style.formBase}>
+            <div className={style.formGroup}>
+              <div className={style.textSmall}>Defina os valores</div>
+              <Grid container>
+                <Grid item xs={5}>
+                  <input type="text" placeholder="0.0000" className={style.inputDefault} />
+                </Grid>
+                <Grid item xs={2}>
+                  <ArrowForward className={style.arrowPrice} />
+                </Grid>
+                <Grid item xs={5}>
+                  <input type="text" placeholder="R$0,00" className={style.inputDefault} />
+                </Grid>
+              </Grid>
+            </div>
+
+            <div className={style.formGroup}>
+              <div className={style.textSmall}>Moeda desejada</div>
+              <Grid container>
+                <Grid item xs={5}>
+                  <Select
+                    list={coinsExample}
+                    title={title}
+                    titleImg={img}
+                    selectItem={this.coinSelected}
+                    error={null}
+                    width={"100%"}
+                  />
+                </Grid>
+                <Grid item xs={7}>
+                  SELECT 
+                </Grid>
+              </Grid>
+
+              <hr />
+            </div>
+
+            <div className={style.formGroup}>
+              <div className={style.textSmall}>Método de negociação</div>
+              <Grid container>
+                <Grid item xs={6}>
+                  P2P (Peer to Peer)
+                </Grid>
+                <Grid item xs={6}>
+                  SCROOW 
+                </Grid>
+              </Grid>
+
+              <hr />
             </div>
             
-          </Grid>
-
-          <Grid item xs={2} />
-          <Grid item xs={10} className={style.boxDetails} >
-            <div className={style.textDetails}>
-              Pagamento em Real pelo BANCO INTER, SANTANDER OU NUBANK
+            <div className={style.formGroup}>
+              <div className={style.textSmall}>Endereço Carteira</div>
+              <input type="text" placeholder="aksdlasd6asd5asd5" className={style.inputDefault} />
+            </div>  
+            <div className={style.formGroup}>
+              <div className={style.textSmall}>E-mail</div>
+              <input type="text" placeholder="email@email.com" className={style.inputDefault} />
             </div>
-            <button className={style.btContinue}>Negociar</button>
-          </Grid>
+            <div className={style.formGroup}>
+              <div className={style.textSmall}>Descrição</div>
+              <textarea className={style.textArea}>
+                Pagamento em Real pelo BANCO INTER, SANTANDER OU NUBANK
+              </textarea>
+              <button className={style.btContinue}>CRIAR OFERTA</button>
+            
+            </div>
+          </div>
 
-        </Grid>
       </div>
     );
   }
