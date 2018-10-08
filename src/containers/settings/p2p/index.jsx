0c67dfe6c8133b50a9f0e9b0d28ecd64f5e790b6
Gabriel Sandes <gabriel.sandes@lunes.io>
2018-10-08 16:18:29 -0300

    Added link back to settings

--- src/containers/settings/p2p/index.jsx
+++ src/containers/settings/p2p/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 // MATERIAL UI
 import Grid from "@material-ui/core/Grid";
+import Hidden from "@material-ui/core/Hidden";
 
 // STYLE
 import style from "./style.css";
@@ -13,7 +14,40 @@ import i18n from "../../../utils/i18n";
 class P2P extends React.Component {
   render() {
     return (
-      <div className={style.boxContainer}>
+      <div>
+        <Grid item xs={12} className={style.containerHeaderSettings}>
+          <Grid item xs={12} className={style.headerSettingsDefault}>
+            <Hidden smUp>
+              <Grid item xs={12}>
+                <h3>{i18n.t("SETTINGS_USER")} </h3>
+              </Grid>
+            </Hidden>
+            <Grid item xs={2} />
+
+            <Grid item xs={6} sm={2}>
+              <Link to="settings">
+                <p>{i18n.t("SETTING_LINK_RETURN")}</p>
+              </Link>
+            </Grid>
+            <Hidden xsDown>
+              <Grid item xs={12} sm={3}>
+                <h3>{i18n.t("SETTINGS_USER")}</h3>
+              </Grid>
+            </Hidden>
+
+            <Grid item xs={10} sm={6} id={"hr"}>
+              <hr />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid container>
+          <Grid item className={style.plansDescription}>
+            <h1>P2P</h1>
+            <p>lorem ipsum</p>
+          </Grid>
+        </Grid>
+
         <Grid container className={style.p2pContainer}>
           <Grid item>
             <div className={style.cardP2p}>
@@ -22,7 +56,7 @@ class P2P extends React.Component {
                 src="/images/icons/p2p/card.png"
                 className={style.cardIcon}
               />
-              <div className={style.hr} />
+              <div className={style.hrCard} />
               <div className={style.cardTitle}>
                 <p>
                   O plano básico de P2P te permitirá usar o sistema Lunes de P2P
@@ -46,7 +80,7 @@ class P2P extends React.Component {
                 src="/images/icons/p2p/card.png"
                 className={style.cardIcon}
               />
-              <div className={style.hr} />
+              <div className={style.hrCard} />
               <div className={style.cardTitle}>
                 <p>
                   O plano básico de P2P te permitirá usar o sistema Lunes de P2P
@@ -70,7 +104,7 @@ class P2P extends React.Component {
                 src="/images/icons/p2p/card.png"
                 className={style.cardIcon}
               />
-              <div className={style.hr} />
+              <div className={style.hrCard} />
               <div className={style.cardTitle}>
                 <p>
                   O plano básico de P2P te permitirá usar o sistema Lunes de P2P
@@ -94,7 +128,7 @@ class P2P extends React.Component {
                 src="/images/icons/p2p/card.png"
                 className={style.cardIcon}
               />
-              <div className={style.hr} />
+              <div className={style.hrCard} />
               <div className={style.cardTitle}>
                 <p>
                   O plano básico de P2P te permitirá usar o sistema Lunes de P2P
