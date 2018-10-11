facb57203a45be582ce1123423af095304b4a7f5
Jorge William <jorgewra@gmail.com>
2018-10-10 22:16:59 -0400

    initial

---
+++ src/containers/p2p/components/multiSelect/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+// STYLE
+import style from "./style.css";
+
+class MultiSelect extends React.Component {
+    render() {
+        
+    }
+}
+
+MultiSelect.propTypes = {};
+
+export default MultiSelect;
