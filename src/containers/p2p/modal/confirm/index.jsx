ce89af34a55e01cbc565c398347e3c6b04c92d2a
Gabriel Sandes <gabriel.sandes@lunes.io>
2018-10-11 16:21:08 -0300

    added style for modal

--- src/containers/p2p/modal/confirm/index.jsx
+++ src/containers/p2p/modal/confirm/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-
 // MATERIAL UI
 import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
 
 // UTILS
 import i18n from "../../../../utils/i18n";
@@ -14,28 +14,41 @@ import style from "./style.css";
 //MATERIAL
 
 class ConfirmModal extends React.Component {
-  render(){
+  render() {
     return (
       <div className={style.boxModal}>
-        <Grid container spacing={24} classeName={style.confirmContainer}>
-          
-          
+        <Grid container>
           <Grid item xs={12}>
-            <div className={style.btnModal}>
-              <button className={style.buttonCard}>{i18n.t("P2P_BTN_CONFIRM")}</button>
+            <div className={style.profile}>
+              <Avatar
+                src={"images/lunio/lunio-user@100x100.jpg"}
+                className={style.avatar}
+              />
+              <span>Felipe Mendes</span>
+              <div className={style.hr} />
             </div>
           </Grid>
 
-          
+          <Grid item xs={12}>
+            <div>
+              <span>Classifique o vendedor</span>
+              <span>**ESTRELAS**</span>
+              <textarea name="" id="" cols="30" rows="10" />
+            </div>
+          </Grid>
+
+          <Grid item xs={12}>
+            <div>
+              <button className={style.buttonCard}>Confirmar</button>
+            </div>
+          </Grid>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
-ConfirmModal.propTypes = {
-  
-}
+ConfirmModal.propTypes = {};
 
 export default ConfirmModal;
 
