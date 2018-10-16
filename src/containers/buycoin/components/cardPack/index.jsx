df3aa2cb1ecf31713b3cfdb60e59f418d85db614
Gabriel Sandes <gabriel.sandes@lunes.io>
2018-10-16 16:16:55 -0300

    Value card initial configs

--- src/containers/buycoin/components/cardPack/index.jsx
+++ src/containers/buycoin/components/cardPack/index.jsx
@@ -1,37 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// COMPONENTS 
+// MATERIAL UI
+import Grid from "@material-ui/core/Grid";
 
-// STYLES 
+// STYLES
 import style from "./style.css";
 
 class CardPack extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
-  render(){
+  render() {
     return (
-      <div>
-        <img src="" />
-        <hr />
-        <p>
-          texto aqui 
-        </p>
-        <h3>0.12345678</h3>
-        <h2>
-          <span>R$</span>
-          30
-          <span>,00</span>
-        </h2>
-      </div>
-    )
+      <Grid container>
+        <Grid item>
+          <div className={style.cardBuy}>
+            <img
+              src="/images/icons/coins/lunes.png"
+              className={style.cardIcon}
+            />
+            <div className={style.hrCard} />
+            <p>Você está comprando <b>15,00 Reais</b> e está recebendo</p>
+            <h1 className={style.amount}>500000</h1>
+            <div className={style.valueCard}>
+              <span className={style.dollarSign}>R$</span>
+              <span className={style.value}>15</span>
+              <span className={style.decimals}>,00</span>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
-CardPack.propTypes = {
-
-}
+CardPack.propTypes = {};
 
 export default CardPack;
