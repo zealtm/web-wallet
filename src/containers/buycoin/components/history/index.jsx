5c6b8cc965029ab12977c993a665a0b61fe10e6b
Caio Milleo <caiomilleo@hotmail.com>
2018-10-17 16:35:55 -0300

    atividade V-295

--- src/containers/buycoin/components/history/index.jsx
+++ src/containers/buycoin/components/history/index.jsx
@@ -7,21 +7,27 @@ import HistoryCard from "../historyCard";
 // STYLE 
 import style from "./style.css";
 
+import { Grid } from "@material-ui/core";
+
 class History extends React.Component {
 
   renderHistory = () => {
-    return [0,1,2,3].map((val,index)=>{
-       return (
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19].map((val, index) => {
+      return (
         <HistoryCard key={index} />
-       )
+      )
     })
   }
 
-  render(){
-    return(
-        <div className={style.boxContainer}>
-          {this.renderHistory()}
-        </div>
+  render() {
+    return (
+      <Grid container direction="row" justify="center">
+        <Grid item xs={12} md={6} className={style.box}>
+          <div className={style.boxContainer}>
+            {this.renderHistory()}
+          </div>
+        </Grid>
+      </Grid>
     )
   }
 }
--- src/containers/buycoin/components/historyCard/index.jsx
+++ src/containers/buycoin/components/historyCard/index.jsx
@@ -2,31 +2,50 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // COMPONENTS 
-import {Grid} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 
 // STYLE 
 import style from "./style.css";
 
 class HistoryCard extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <h3>Nome do Usuario Aqui</h3>
-        <Grid container>
+        <Grid container className={style.maginText}>
+
           <Grid item xs={8}>
-            <span>22/12/2018 17:30</span>
-            <span>Confirmado</span>
-            <br />
-            Credito 
-            <br />
-            ID - 12345678
+
+            <Grid direction="row" className={style.maginText}>
+              <span className={style.bold}>22/12/2018 17:30</span>
+              <span className={style.status}>Confirmado</span>
+            </Grid>
+
+            <Grid direction="row" className={style.maginText}>
+              <span className={style.text}>Crédito</span>
+            </Grid>
+
+            <Grid direction="row" className={style.maginText}>
+              <span className={style.text}>ID - 12345678</span>
+            </Grid>
+
           </Grid>
+
           <Grid item xs={4}>
-            <img src="" /> Lunes
-            <span>0.12345678</span>
-            R$ 0,00
+
+            <Grid direction="row" className={style.maginTextRight}>
+              <img src="" /><span className={style.img}>Lunes</span>
+            </Grid>
+
+            <Grid direction="row" className={style.maginTextRight}>
+              <span className={style.lunes}>0.12345678 LUNES</span>
+            </Grid>
+
+            <Grid direction="row" className={style.maginTextRight}>
+              <span className={style.text}>R$ 0,00</span>
+            </Grid>
           </Grid>
         </Grid>
+        <div className={style.line} />
       </div>
     )
   }
