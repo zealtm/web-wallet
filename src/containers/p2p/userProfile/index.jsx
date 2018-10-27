366d4787246f244a208192a1e3916bfd08d0124a
Jorge William <jorgewra@gmail.com>
2018-10-26 23:25:11 -0400

    ajuste layout

--- src/containers/p2p/userProfile/index.jsx
+++ src/containers/p2p/userProfile/index.jsx
@@ -3,24 +3,44 @@ import PropTypes from "prop-types";
 
 //MATERIAL
 import Grid from "@material-ui/core/Grid";
+import LinearProgress from '@material-ui/core/LinearProgress';
 import Avatar from "@material-ui/core/Avatar";
-
+import { withStyles } from '@material-ui/core/styles';
+import colors from "../../../components/bases/colors";
 // styles
 import style from "../style.css";
-
+import StarVotes from "../components/starvotes";
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  colorPrimary: {
+    backgroundColor: colors.purple.dark,
+  },
+  barColorPrimary: {
+    backgroundColor: colors.green.default,
+  },
+};
 class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render(){
+    const { classes } = this.props;
     return (
       <Grid container className={style.baseUserProfile}>
         <Grid item xs={12} sm={12}>
           <div className={style.cardProfile}>
             <div className={style.userInfo}>
+             
               <img
                 src={"images/lunio/lunio-user@100x100.jpg"}
                 className={style.avatarProfile}
               />
+               <div className={style.online} ></div>
               <p className={style.userName}>
-                Felipe Mendes <br />{" "}
+                Felipe Mendes <br />
+                <StarVotes votes={4} />
                 <span className={style.textSmall}>
                   Usuário desde 12/10/1998
                 </span>{" "}
@@ -30,7 +50,7 @@ class UserProfile extends React.Component {
             <div className={style.userDescription}>
               <span className={style.spanDescription}>Descrição</span>
               <div className={style.textDescription}>
-                <p>O usuário com mais numeros de trades!</p>
+                <p>3º maior Node da rede e faço negociação na plataforma desde 2015.</p>
               </div>
             </div>
           </div>
@@ -45,19 +65,21 @@ class UserProfile extends React.Component {
 
             <div className={style.bars}>
               <span className={style.spanBars}>Negociações</span>
+              
               <div className={style.barsNumbers}>
                 <span>+500</span>
               </div>
             </div>
-            <div className={style.bar} />
+            <LinearProgress className={style.bar} classes={{ colorPrimary: classes.colorPrimary, barColorPrimary: classes.barColorPrimary }} value={100} variant="determinate"/>
 
             <div className={style.bars}>
               <span className={style.spanBars}>Concluídas</span>
+              
               <div className={style.barsNumbers}>
                 <span>50%</span>
               </div>
             </div>
-            <div className={style.bar} />
+            <LinearProgress className={style.bar} classes={{ colorPrimary: classes.colorPrimary, barColorPrimary: classes.barColorPrimary }} value={50} variant="determinate"  />
           </div>
         </Grid>
 
@@ -80,5 +102,6 @@ class UserProfile extends React.Component {
     )
   }
 }
+UserProfile.propTypes = {}
 
-export default UserProfile;
+export default withStyles(styles) (UserProfile);
