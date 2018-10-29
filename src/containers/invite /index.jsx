23bcc4081752b96169ee8b7fb59d49eb1046e12e
Thiago Pereira <thiago_salper@hotmail.com>
2018-10-29 17:21:39 -0300

    invite init

---
+++ src/containers/invite /index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+//STYLE 
+import style from "./style.css";
+
+class Invite extends React.Component {
+  render(){
+    return (
+      <div>Convite</div>
+    )
+  }
+}
+
+export default Invite;
