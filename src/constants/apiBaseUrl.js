49042aef6ef6a3cf89fe62fa42756a2373bb5771
Hellen <hellen.ferreira@lunes.io>
2018-10-29 16:45:33 -0300

    links blockexplorer

--- src/constants/apiBaseUrl.js
+++ src/constants/apiBaseUrl.js
@@ -1,6 +1,10 @@
-export const TESTNET = true;
-export const BASE_URL = TESTNET ? "https://a.lunes.io/wallet/staging" : "https://a.lunes.io/wallet/v1";
-export const LUNESNODE_URL = TESTNET ? "https://lunesnode-testnet.lunes.io" : "https://lunesnode.lunes.io";
+export const TESTNET = !true;
+export const BASE_URL = TESTNET
+  ? "https://a.lunes.io/wallet/staging"
+  : "https://a.lunes.io/wallet/v1";
+export const LUNESNODE_URL = TESTNET
+  ? "https://lunesnode-testnet.lunes.io"
+  : "https://lunesnode.lunes.io";
 export const TETHER_URL = "https://api.omniwallet.org";
 export const HEADER_RESPONSE = "x-amzn-remapped-authorization";
 export const HEADER_REQUEST = {
@@ -19,7 +23,17 @@ export const HEADER_REQUEST_FORM = {
 
 export const API_HEADER = {
   headers: {
-    key:
-      "IIP0X6S4Ui7z0lTfTkeLO6te2ZmyxOJ1fNeuoIC9"
+    key: "IIP0X6S4Ui7z0lTfTkeLO6te2ZmyxOJ1fNeuoIC9"
   }
+};
+
+export const blockexplorer = {
+  lunes: TESTNET
+    ? "https://blockexplorer-testnet.lunes.io/tx/"
+    : "https://blockexplorer.lunes.io/tx/",
+  btc: "https://live.blockcypher.com/btc/tx/",
+  ltc: "https://live.blockcypher.com/ltc/tx/",
+  bch: "https://live.blockcypher.com/bch/tx/",
+  dash: "https://chainz.cryptoid.info/dash/search.dws?q=",
+  eth: "https://etherscan.io/tx/"
 };
