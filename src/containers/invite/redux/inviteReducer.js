4ceb9caa55e6e9c286f8015fa833937c9474f21e
Jorge William <jorgewra@gmail.com>
2018-10-31 22:16:19 -0400

    Redux initial

---
+++ src/containers/invite/redux/inviteReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    reload: false,
+};
+const invite = (state = initialState, action) => {
+    switch (action.type) {
+        default: {
+            return {
+              ...state
+            };
+        }
+    }
+};
+export default invite;
