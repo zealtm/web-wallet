bf5021f182e58c77e8beaad7f6f1b8d4f2b83b49
Jorge William <jorgewra@gmail.com>
2018-11-01 21:28:28 -0400

    Event copy item

--- src/containers/invite/components/itemInvite/index.jsx
+++ src/containers/invite/components/itemInvite/index.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// REDUX
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { successRequest } from "../../../errors/redux/errorAction";
+
 // MATERIAL UI
 import { Grid } from "@material-ui/core";
 import Avatar from "@material-ui/core/Avatar";
+
+// UTILS
+import i18n from "../../../../utils/i18n";
 
 // STYLE
 import style from "./style.css";
 
 class ItemInvite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  copyAddress = (address) => {
+    let { successRequest } = this.props;
+    const element = document.createElement("textarea");
+    element.value = address;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand("copy");
+    document.body.removeChild(element);
+    successRequest(i18n.t("MODAL_RECEIVE_MESSAGE"));
+  };
+
   render() {
     return (
       <div>
@@ -27,6 +49,7 @@ class ItemInvite extends React.Component {
           </Grid>
           <Grid item xs={2} sm={2}>
             <img
+              onClick = {() => this.copyAddress('f5234s3f5v4sd3fg54v3df5g43d5fg43dsf53543')}
               className={style.imgCopy}
               src="/images/icons/modal-receive/ic_copy@1x.png"
             />
@@ -41,7 +64,14 @@ class ItemInvite extends React.Component {
 }
 
 ItemInvite.propTypes = {
-  //
+  successRequest: PropTypes.func
 };
+const mapStateToProps = store => ({
+});
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ successRequest }, dispatch);
 
-export default ItemInvite;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+) (ItemInvite);
