b09a0f3bef44d111a825c7c0885a68b22fe8411d
Thiago Pereira <thiago_salper@hotmail.com>
2018-11-06 15:23:30 -0200

    ajusts

--- src/components/actionButton.jsx
+++ src/components/actionButton.jsx
@@ -5,27 +5,25 @@ import PropTypes from "prop-types";
 import style from "./style.css";
 
 class ActionButton extends React.Component {
-    actionButton = () => {
-        const { action , content} = this.props;
-        action(content);
-    };
+  render() {
+    const { title, typeAction, action } = this.props;
+    const icon =
+      typeAction == "copy"
+        ? "/images/icons/modal-receive/ic_copy@1x.png"
+        : "/images/icons/invite/share@1x.png";
 
-    render() {
-        const { title, typeAction } = this.props;
-
-        return (
-            <div onClick={() => this.actionButton()} className={style.imgAction}>
-                {typeAction == 'copy' ? <img src="/images/icons/modal-receive/ic_copy@1x.png" /> : <img src="/images/icons/invite/share@1x.png" />}
-                <p>{title ? title : ""}</p>
-            </div>
-        );
-    }
+    return (
+      <div onClick={action} className={style.imgAction}>
+        <img src={icon} />
+        <p>{title ? title : ""}</p>
+      </div>
+    );
+  }
 }
 ActionButton.propTypes = {
-    action: PropTypes.func,
-    typeAction: PropTypes.oneOf(['share', 'copy']).isRequired,
-    content: PropTypes.string.isRequired,
-    title: PropTypes.string,
-}
+  action: PropTypes.func,
+  typeAction: PropTypes.oneOf(["share", "copy"]).isRequired,
+  title: PropTypes.string
+};
 
 export default ActionButton;
