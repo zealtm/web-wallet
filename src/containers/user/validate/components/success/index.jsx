aad33193b38d6a9421e325bfa347f66c52732de8
Gabriel Sandes <gabriel.sandes@lunes.io>
2018-12-04 16:39:52 -0200

    adjusted email verification screen

--- src/containers/user/validate/components/success/index.jsx
+++ src/containers/user/validate/components/success/index.jsx
@@ -4,6 +4,9 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
+// UTILS
+import i18n from "../../../../../utils/i18n";
+
 // STYLE
 import style from "./style.css";
 
@@ -11,10 +14,15 @@ class EmailSuccess extends React.Component {
   render() {
     return (
       <div className={style.baseMessage}>
-        <img src="../../images/logo.svg" className={style.icon} />
-        <p className={style.messageSuccess}>E-mail confirmado.</p>
-        <Link to="/login" className={style.buttonLogin}>
-          Login
+        <img
+          src="../../images/icons/confirm/confirm@2x.png"
+          className={style.icon}
+        />
+        <p className={style.messageSuccess}>
+          {i18n.t("SETTINGS_USER_EMAIL_VERIFIED")}
+        </p>
+        <Link to="/wallet">
+          <button className={style.buttonLogin}>{i18n.t("BTN_LOGIN")}</button>
         </Link>
       </div>
     );
