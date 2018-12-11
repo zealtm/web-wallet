2f8b2f26d5fbe121ca8d691f4dd09f1526c14986
Thiago Pereira <thiago_salper@hotmail.com>
2018-12-11 17:26:36 -0200

    clear code unused

--- src/containers/p2p/modal/sellConfirm/index.jsx
+++ src/containers/p2p/modal/sellConfirm/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 // UTILS
 import i18n from "../../../../utils/i18n";
@@ -7,24 +6,26 @@ import i18n from "../../../../utils/i18n";
 // STYLE
 import style from "./style.css";
 
-//COMPONENTS
-
 class SellConfirmModal extends React.Component {
   render() {
-    return <div className={style.containerSellConfirm}>
-        <img src="/images/modal/sell-confirm.png" className={style.imgSellConfirm} />
+    return (
+      <div className={style.containerSellConfirm}>
+        <img
+          src="/images/modal/sell-confirm.png"
+          className={style.imgSellConfirm}
+        />
         <div>
-        <p className={style.textSellConfirm}> {i18n.t("P2P_TEXT_13")}
-          </p>
+          <p className={style.textSellConfirm}> {i18n.t("P2P_TEXT_13")}</p>
         </div>
         <div className={style.boxBtnSellConfirm}>
-        <button className={style.btnSellConfirm}> {i18n.t("P2P_BUTTON_CONFIRM_RECEIPT")}
+          <button className={style.btnSellConfirm}>
+            {" "}
+            {i18n.t("P2P_BUTTON_CONFIRM_RECEIPT")}
           </button>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
-
-SellConfirmModal.propTypes = {};
 
 export default SellConfirmModal;
