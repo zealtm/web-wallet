09a9f7bda601955db4b9eddb64b798e18f6fb323
Pedro Salgado <pedro.salgado@lunes.io>
2019-01-07 17:52:10 -0200

    Add new component button and information

--- src/containers/deposit/invoice.jsx
+++ src/containers/deposit/invoice.jsx
@@ -13,8 +13,18 @@ class Invoice extends React.Component {
   render() {
     return (
       <Grid container direction="row" justify="center">
-        <Grid item xs={12} className={style.box} style={{ padding: 5 }}>
-          <p>{i18n.t("DEPOSIT_TAB_TITLE")}</p>
+        <Grid item xs={12} className={style.transparentBox} style={{ marginTop: "10px" }}>
+          <button className={style.buttonBorderGreen}>
+            {i18n.t("DEPOSIT_TAB_TITLE")}
+          </button>   
+        </Grid>
+        <Grid item xs={12} className={style.transparentBox} style={{marginTop: "10px"}}>
+            <div className={style.information}>
+                <a href="#">
+                {i18n.t("COUPON_INSTRUCTIONS")}
+                <img src="/images/icons/recharge/ic_instrucoes.png" alt= {i18n.t("COUPON_INSTRUCTIONS")}/>
+                </a>
+            </div>
         </Grid>
       </Grid>
     );
