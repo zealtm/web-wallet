ce6396bfb3d113d90bcd64947da40b574c98ab46
Adelson dos Santos <adelsondcristo@hotmail.com>
2019-01-30 16:34:44 -0200

    V-648 Modal payment deposit

--- src/containers/deposit/modal/payModal/index.jsx
+++ src/containers/deposit/modal/payModal/index.jsx
@@ -1,36 +1,80 @@
 import React, { Component } from "react";
 import style from "./style.css";
 import i18n from "../../../../utils/i18n";
-import { Grid } from "@material-ui/core";
+import { Grid, Hidden } from "@material-ui/core";
 
 class PayModal extends Component {
   render() {
     return (
-      <Grid container className={style.container}>
-        <Grid item xs={12} sm={12} style={{ textAlign: "center" }}>
+      <Grid item xs={12} sm={12} container className={style.containerPayModal}>
+        <Grid item xs={12} sm={12}>
           <img
+            className={style.payModalImg}
             src="images/icons/deposit/confirm-ticket.png"
-            alt={i18n.t("BANK_ICON")}
+            alt={i18n.t("DEPOSIT_TAB_TITLE")}
           />
         </Grid>
 
         <Grid item xs={12} sm={12}>
-          <p
-            style={{
-              textAlign: "center",
-              color: "#ffff",
-              margin: "0 25% 0 25% 0"
-            }}
-          >
-            Boleto gerado e enviado para o e-mail cadastrado. Após o pagamento,
-            o dinheiro estará disponível para uso na plataforma em até 2 dias
-            úteis
+          <p className={style.payModalText} style={{ marginBottom: 40 }}>
+            {i18n.t("DEPOSIT_PAYMODAL_TEXT")}
           </p>
         </Grid>
+
+        <Grid item xs={12} sm={12} className={style.payModalField}>
+          <span>1232154646465465000000213210013</span>
+        </Grid>
+
         <Grid item xs={12} sm={12}>
-          <span className={style.ConfirmDataField}>
-            {i18n.t("DEPOSIT_CONFIRMDATA_NAME")}
+          <span className={style.payModalTitle}>
+            {i18n.t("DEPOSIT_PAYMODAL_TITLE")}
           </span>
+        </Grid>
+
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          className={style.payModalBoxImgIcon}
+          style={{ marginTop: 25 }}
+        >
+          <Grid>
+            <a href="#">
+              <img
+                src="/images/icons/deposit/paymodal_copy@1x.png"
+                className={style.payModalIconsCopy}
+              />
+              <p className={style.payModalTextIcon}>
+                {i18n.t("DEPOSIT_PAYMODAL_COPY")}
+              </p>
+            </a>
+          </Grid>
+
+          <Grid className={style.payModalIconsBoxDownload}>
+            <a href="#">
+              <img
+                src="/images/icons/deposit/paymodal_download@1x.png"
+                className={style.payModalIconsDownload}
+              />
+              <p className={style.payModalTextIcon}>
+                {i18n.t("DEPOSIT_PAYMODAL_DOWNLOAD")}
+              </p>
+            </a>
+          </Grid>
+
+          <Hidden smUp>
+            <Grid>
+              <a href="#">
+                <img
+                  src="/images/icons/deposit/paymodal_share@1x.png"
+                  className={style.payModalIconsShare}
+                />
+                <p className={style.payModalTextIcon}>
+                  {i18n.t("DEPOSIT_PAYMODAL_SHARE")}
+                </p>
+              </a>
+            </Grid>
+          </Hidden>
         </Grid>
       </Grid>
     );
