6175badf941a2d9491fda9558f4c65b931526cf9
Pedro Salgado <pedro.salgado@lunes.io>
2019-04-04 00:41:31 -0300

    Fixed deposit state field

--- src/containers/deposit/modal/informations/index.jsx
+++ src/containers/deposit/modal/informations/index.jsx
@@ -141,7 +141,6 @@ class InformationModal extends React.Component {
       document: "",
       city: "",
       state: "",
-      stateName: "",
       cep: "",
       address: "",
       addressNumber: "",
@@ -178,7 +177,6 @@ class InformationModal extends React.Component {
             documentType && documentType !== "passport" ? documentType : "",
           document: document && documentType !== "passport" ? document : "",
           state: state ? state : "",
-          stateName:state ? state : "",
           city: city ? city : "",
           cep: zipcode ? zipcode : "",
           address: street ? street : ""
@@ -212,17 +210,17 @@ class InformationModal extends React.Component {
       fullName && document && state && city && cep && address
     );
   };
-  searchStates = () => {
+
+  searchStatesName = (value) => {
     const { states } = this.props;
-    const { stateName } = this.state;
-    let shortName = null;
+    let name = null;
     states.forEach((element, index) => {
-      if (element.name === stateName) {
-        shortName = element.shortName;
+      if (element.shortName === value) {
+        name = element.name;
       }
     });
-    if (shortName !== null) {
-      return shortName;
+    if (name !== null) {
+      return name;
     }
     return;
   };
@@ -243,7 +241,7 @@ class InformationModal extends React.Component {
       case "state":
         value = e.target.value;
         depositGetCity({ country: "BR", state: value });
-        this.setState({stateName: value, city: ""});
+        this.setState({state: value, city: ""});
         break;
       default:
         value = e.target.value;
@@ -265,12 +263,11 @@ class InformationModal extends React.Component {
       cep,
       address
     } = this.state;
-    let shortName = this.searchStates();
     let user = {
       fullName,
       documentType,
       document,
-      state:shortName,
+      state,
       city,
       cep,
       address
@@ -289,7 +286,7 @@ class InformationModal extends React.Component {
     if (states) {
       return states.map((item, index) => (
         <MenuItem
-          value={item.name}
+          value={item.shortName}
           key={index}
           classes={{
             root: classes.menuItemRoot
@@ -326,7 +323,6 @@ class InformationModal extends React.Component {
       disabled,
       statusKyc,
       checkInputs,
-      stateName
     } = this.state;
     const MenuProps = {
       PaperProps: {
@@ -490,7 +486,7 @@ class InformationModal extends React.Component {
                 classes={{ selectMenu: classes.underlineItems }}
                 MenuProps={MenuProps}
                 value={this.state.state}
-                renderValue={value => value}
+                renderValue={value => this.searchStatesName(value)}
                 input={
                   <Input
                     classes={{
